308f9fea806c76d2d0a758301709e18d857d3932
chung5566 <100703031@nccu.edu.tw>
2015-06-11 23:39:59 +0800

    jquery and handlebar

--- news/news.js
+++ news/news.js
@@ -1,10 +1,6 @@
 Parse.initialize("r5t5aNmsQptsKzXchxmm6RezYVZTO1RnniaLUChx", "ldT9P8aBQBePGNhlIKlksHWV4BKVP6VTs9xgIao5");
 
-var TestObject = Parse.Object.extend("TestObject");
-var testObject = new TestObject();
-testObject.save({foo: "bar"}).then(function(object) {
-  alert("yay! it worked");
-});
+
 
 var News = Parse.Object.extend("News");
 
@@ -13,24 +9,29 @@ var Newss = Parse.Collection.extend({
 });
 
 var newss = new Newss()
-var NewsView =  Parse.View.extend({
-    template: Handlebars.compile($('#blogs-tpl').html()),
-    render: function(){ 
-        var collection = { newss: this.collection.toJSON() };
-        this.$el.html(this.template(collection));
-    }
-});
 
 newss.fetch({
     success: function(newss) {
-    var NewsView = new NewsView({ collection: newss });
-    NewsView.render();
-    $('.main-container').html(NewsView.el);
-	},
+    var newssView = new NewssView({ collection: newss });
+    newssView.render();
+    $('.main-container').html(newssView.el);
+},
     error: function(newss, error) {
         console.log(error);
     }
 });
+
+
+
+
+var NewssView =  Parse.View.extend({
+    template : Handlebars.compile($("#blogs-tpl").html()),
+    render: function(){ 
+        var collection = { news: this.collection.toJSON() };
+        this.$el.html(this.template(collection));
+    }
+});
+
 
 
 
